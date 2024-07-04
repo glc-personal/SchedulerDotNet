47d5e7ba8ccbe5960ba93db7f8d0faef8cbc860b
glc-personal <gabriel.lopez.candales@gmail.com>
2024-07-04 10:48:32 -0400

    Setup the Team view structure and added a Descriptor partial view for reuse.

--- SchedulerDotNet/Controllers/HomeController.cs
+++ SchedulerDotNet/Controllers/HomeController.cs
@@ -32,6 +32,11 @@ namespace SchedulerDotNet.Controllers
             return View(weekDays);
         }
 
+        public IActionResult Team()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
