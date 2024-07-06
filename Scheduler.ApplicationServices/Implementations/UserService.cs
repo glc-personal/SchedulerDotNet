18d4e072ee6a4cf078fac6b70941f5a4d60cf556
glc-personal <gabriel.lopez.candales@gmail.com>
2024-07-06 13:47:21 -0400

    Added the initial migration to the database for user and title tables. Setup the User Service interface and implementation. Modified the TeamViewModel to hold its Descriptor ViewModel since Team View can only have one model. Setup a folder for the Team view (will need to do this for Preferences as well).

---
+++ Scheduler.ApplicationServices/Implementations/UserService.cs
@@ -0,0 +1,21 @@
+﻿using Scheduler.Core.Models;
+using Scheduler.ApplicationServices.Interfaces;
+using Scheduler.DataAccess;
+
+namespace Scheduler.ApplicationServices.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly SchedulerDbContext _context;
+
+        public UserService(SchedulerDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
