44514777d1d43a1757d373b06bc22382ca9e222f
glc-personal <gabriel.lopez.candales@gmail.com>
2024-07-11 12:07:06 -0400

    Small Updated the UserService to allow for obtaining a user by their id. Added a new HowTo for adding a new view. Setup the Metric vie, viewmodel and controller for testing.

---
+++ SchedulerDotNet/Controllers/MetricsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Scheduler.ApplicationServices.Interfaces;
+using Scheduler.Core.Models;
+using SchedulerDotNet.Models;
+
+namespace SchedulerDotNet.Controllers
+{
+    public class MetricsController : Controller
+    {
+
+        private readonly ILogger<MetricsController> _logger;
+        private readonly IUserService _userService;
+
+        public MetricsController(ILogger<MetricsController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Initialize the view model for this view
+            MetricsViewModel viewModel = new MetricsViewModel();
+            // Get the user (fixed to a single user id = 2 for now, need to change when login is featured)
+            viewModel.User = await _userService.GetUserByIdAsync(2);
+            if (viewModel.User == null)
+            {
+                return View("Error");
+            }
+
+            // Setup the View's Right Section 
+            SetupRightSection(viewModel);
+
+            // Setup the View's Left Section
+            SetupLeftSection(viewModel);
+
+            return View(viewModel);
+        }
+
+        private void SetupRightSection(MetricsViewModel viewModel)
+        {
+            User user = viewModel.User;
+            // Setup the Profile Stamp for this user
+            ProfileStampViewModel profileStamp = new ProfileStampViewModel
+            {
+                ProfilePicturePath = user.ProfilePicturePath,
+                Name = user.FirstName + " " + user.LastName,
+                Title = user.Title,
+            };
+            viewModel.ProfileStamp = profileStamp;
+        }
+
+        private void SetupLeftSection(MetricsViewModel viewModel)
+        {
+            User user = viewModel.User;
+            DescriptorViewModel overviewDescriptor = new DescriptorViewModel
+            {
+                Title = "Overview",
+                Description = "Below you can find small summary metrics and information regarding your performance."
+            };
+            viewModel.OverviewDescriptor = overviewDescriptor;
+            DescriptorViewModel hoursWorkedDescriptor = new DescriptorViewModel
+            {
+                Title = "Hours Worked",
+                Description = "Below you can find a visual representation of your hours worked."
+            };
+            viewModel.HoursWorkedDescriptor = hoursWorkedDescriptor;
+        }
+    }
+}
---
+++ SchedulerDotNet/Models/MetricsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Scheduler.Core.Models;
+
+namespace SchedulerDotNet.Models
+{
+    public class MetricsViewModel
+    {
+        public User User { get; set; }
+        public ProfileStampViewModel ProfileStamp { get; set; }
+        public DescriptorViewModel OverviewDescriptor { get; set; }
+        public DescriptorViewModel HoursWorkedDescriptor { get; set; }
+    }
+}
